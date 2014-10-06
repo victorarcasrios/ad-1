a0d0c0b73218f7229c25203ec5d511eb042a4e34
juankza <cazalillacostaj@gmail.com>
2014-10-06 08:32:55 +0200

    Cambios

---
+++ PCategoria/PCategoria/EditWindow.cs
@@ -0,0 +1,21 @@
+//gitUser> juankza
+
+using Gtk;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+namespace PCategoria
+{
+	public partial class EditWindow : Gtk.Window
+	{
+		public EditWindow () : base(Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+
+
+
+		}
+	}
+}
+
